d6712a433eb9da29c26c57fe3d9af42d72c66611
Youssef Hussein <youssef.hussein.business@gmail.com>
2024-10-23 15:27:03 +0300

    fix portfolio photo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1047769673"
+    serviceWorkerVersion: "1812105069"
   }
 });
